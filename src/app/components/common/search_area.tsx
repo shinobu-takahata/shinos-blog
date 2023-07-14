b76a66f5ee034ebdb3c81d5b5492b3b16b8219f9
shinobu_takahata <shinoshino2335@gmail.com>
2023-07-15 06:35:37 +0900

    レイアウト調整

--- src/app/components/common/search_area.tsx
+++ src/app/components/common/search_area.tsx
@@ -8,12 +8,15 @@ const SearchArea = () => {
     const handlesearchAreaOpen = usesearchAreaOpenStore((state) => state.handlesearchAreaOpen);
 
     return (
-        <div className={searchAreaOpen ? "fixed top-0 w-full h-full bg-black bg-opacity-60 z-50" : ""}>
+        <div className={searchAreaOpen ? "fixed top-0 w-full h-full bg-black bg-opacity-60 z-50" : "invisible"}>
             <div className="mt-24 flex flex-col justify-center items-center">
-                <div className="w-64 flex justify-end">
+                <div className="w-96 flex justify-end">
                     <FontAwesomeIcon icon={faXmark} size="2xl" style={{color: "#fafafa",}} onClick={() => handlesearchAreaOpen(searchAreaOpen)} className="hover:cursor-pointer transition-all duration-150"/> 
                 </div>
-                <input type="text" className="w-64"/>
+                <div className="flex">
+                    <input type="text" className="w-64 mr-2"/>
+                    <button type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">OK</button>
+                </div>
             </div>
         </div>
     );
